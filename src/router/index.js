57eafceff2d61fa227f65d527be8229ed1c7b529
lulin <286922169@qq.com>
2020-01-08 15:30:46 +0800

    冲突解决

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// const page1  = ()=>{ import  '../views/page1'};
-// const page2  = ()=>{ import  ('../views/page2')};
-// const page3  = ()=>{ import  (  '../views/page3')};
-// const HelloWorld  = ()=>{ import  ( '../components/HelloWorld')};
-// const mainPage  = ()=>{ import  ( '../views/mainPage')};
-import page1 from '../views/page1'
-import page2 from '../views/page2'
-import HelloWorld from '../components/HelloWorld'
-import mainPage from '../views/mainPage'
+const page1  = ()=>{ import  '../views/page1'};
+const page2  = ()=>{ import  ('../views/page2')};
+const page3  = ()=>{ import  (  '../views/page3')};
+const HelloWorld  = ()=>{ import  ( '../components/HelloWorld')};
+const mainPage  = ()=>{ import  ( '../views/mainPage')};
+// import page1 from '../views/page1'
+// import page2 from '../views/page2'
+// import HelloWorld from '../components/HelloWorld'
+// import mainPage from '../views/mainPage'
 
 
 Vue.use(Router);
